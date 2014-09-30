dfd25eb18681cd0eb8d73c4b1c32eccd359acb51
Wilson da Silva Moura <wilsonsmoura@gmail.com>
2014-09-30 17:03:08 -0300

    Placing single quotes around array keys.

--- settings.php
+++ settings.php
@@ -16,35 +16,35 @@ function multimedia_page_admin() {
                     <tbody>
                         <tr valign="top">
                             <th scope="row"><?php _e('Service URL', 'multimedia'); ?>:</th>
-                            <td><input type="text" name="multimedia_config[service_url]" value="<?php echo ($config['service_url'] == '' ? 'http://fi-admin.bvsalud.org/' : $config['service_url'] ) ?>" class="regular-text code"></td>
+                            <td><input type="text" name="multimedia_config['service_url']" value="<?php echo ($config['service_url'] == '' ? 'http://fi-admin.bvsalud.org/' : $config['service_url'] ) ?>" class="regular-text code"></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php _e('Plugin page', 'multimedia'); ?>:</th>
-                            <td><input type="text" name="multimedia_config[plugin_slug]" value="<?php echo ($config[plugin_slug] != '' ? $config[plugin_slug] : 'multimedia'); ?>" class="regular-text code"></td>
+                            <td><input type="text" name="multimedia_config['plugin_slug']" value="<?php echo ($config['plugin_slug'] != '' ? $config['plugin_slug'] : 'multimedia'); ?>" class="regular-text code"></td>
                         </tr>
 
                         <tr valign="top">
                             <th scope="row"><?php _e('Filter query', 'multimedia'); ?>:</th>
-                            <td><input type="text" name="multimedia_config[initial_filter]" value='<?php echo $config[initial_filter] ?>' class="regular-text code"></td>
+                            <td><input type="text" name="multimedia_config['initial_filter']" value='<?php echo $config['initial_filter'] ?>' class="regular-text code"></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php _e('Search form', 'multimedia'); ?>:</th>
                             <td>
-                                <input type="checkbox" name="multimedia_config[show_form]" value="1" <?php if ( $config['show_form'] == '1' ): echo ' checked="checked"'; endif;?> >
+                                <input type="checkbox" name="multimedia_config['show_form']" value="1" <?php if ( $config['show_form'] == '1' ): echo ' checked="checked"'; endif;?> >
                                 <?php _e('Show search form', 'multimedia'); ?>
                             </td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php _e('Disqus shortname', 'multimedia'); ?>:</th>
-                            <td><input type="text" name="multimedia_config[disqus_shortname]" value="<?php echo $config[disqus_shortname] ?>" class="regular-text code"></td>
+                            <td><input type="text" name="multimedia_config['disqus_shortname']" value="<?php echo $config['disqus_shortname'] ?>" class="regular-text code"></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php _e('AddThis profile ID', 'multimedia'); ?>:</th>
-                            <td><input type="text" name="multimedia_config[addthis_profile_id]" value="<?php echo $config[addthis_profile_id] ?>" class="regular-text code"></td>
+                            <td><input type="text" name="multimedia_config['addthis_profile_id']" value="<?php echo $config['addthis_profile_id'] ?>" class="regular-text code"></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php _e('Google Analytics code', 'multimedia'); ?>:</th>
-                            <td><input type="text" name="multimedia_config[google_analytics_code]" value="<?php echo $config[google_analytics_code] ?>" class="regular-text code"></td>
+                            <td><input type="text" name="multimedia_config['google_analytics_code']" value="<?php echo $config['google_analytics_code'] ?>" class="regular-text code"></td>
                         </tr>                        
                     </tbody>
                 </table>
