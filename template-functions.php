5ea932a23a0f18329abea4ade05577ecce819a1c
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2018-10-30 11:56:06 -0300

    Compatibiliza função para evitar conflito com demais plugins. Close #10

--- template-functions.php
+++ template-functions.php
@@ -16,17 +16,24 @@ if ( !function_exists('print_lang_value') ) {
 }
 
 if ( !function_exists('get_lang_value') ) {
-    function get_lang_value($string, $lang_code){
+    function get_lang_value($string, $lang_code, $default_lang_code = 'en'){
         $lang_value = array();
         $occs = preg_split('/\|/', $string);
-        
+
         foreach ($occs as $occ){
-            $lv = preg_split('/\^/', $occ);
+            $re_sep = (strpos($occ, '~') !== false ? '/\~/' : '/\^/');
+            $lv = preg_split($re_sep, $occ);
             $lang = substr($lv[0],0,2);
-            $value = $lv[1];        
-            $lang_value[$lang] = $value;        
+            $value = $lv[1];
+            $lang_value[$lang] = $value;
         }
-        return $lang_value[$lang_code];
+        if ( isset($lang_value[$lang_code]) ){
+            $translated = $lang_value[$lang_code];
+        }else{
+            $translated = $lang_value[$default_lang_code];
+        }
+
+        return $translated;
     }
 }
 
@@ -56,7 +63,7 @@ if ( !function_exists('get_site_meta_tags') ) {
                 $site_title = trim($site_title);
             }
 
-            $site_meta_tags = get_meta_tags($url);        
+            $site_meta_tags = get_meta_tags($url);
             $site_meta_tags['title'] = $site_title;
 
             foreach ($site_meta_tags as $key => $value) {
@@ -81,7 +88,7 @@ if ( !function_exists('real_site_url') ) {
             $current_language = substr( strtolower(get_bloginfo('language')),0,2 );
 
             if ( $mlf_config['default_language'] != $current_language ){
-                $site_url .= '/' . $current_language;    
+                $site_url .= '/' . $current_language;
             }
         }
         // check for polylang plugin
@@ -110,7 +117,7 @@ if ( !function_exists('display_thumbnail') ) {
         if (strpos($link_data['host'],'youtube.com') !== false){
             $service = 'youtube';
             parse_str($link_data['query'], $params);
-            $video_id = $params['v'];                        
+            $video_id = $params['v'];
         }elseif (strpos($link_data['host'],'vimeo.com') !== false) {
             $service = 'vimeo';
             $video_id = $link_data['path'];
